8bc06f1524fc45ad04a0d7724121c4826b36501b
deybacsi <deyprof@gmail.com>
2022-10-17 21:42:04 +0200

    buy/sell working

--- checks.py
+++
@@ -1,4 +0,0 @@
-def checkbalance(threadno):
-    pybot_threads[threadno]["asset1balance"] = client.get_asset_balance(asset=pybot_threads[threadno]["asset1"])["free"]
-    pybot_threads[threadno]["asset2balance"] = client.get_asset_balance(asset=pybot_threads[threadno]["asset2"])["free"]
-    
